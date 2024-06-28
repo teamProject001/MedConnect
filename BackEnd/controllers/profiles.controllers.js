92136866222d8be50c9bf2c5768eeba6ba27128f
yosri-boujnah <yosriboujnah84@gmail.com>
2024-06-28 11:38:13 +0100

    modification in dellet methods

--- BackEnd/controllers/profiles.controllers.js
+++ BackEnd/controllers/profiles.controllers.js
@@ -8,7 +8,7 @@ const AddProfile = async (req, res) => {
         ProfileModel.findOne({ user: req.body._id })
             .then(async (profile) => {
                 if (!profile) {
-                    //req.body.user = req.user.id
+                  
                     await ProfileModel.create(req.body)
                     res.status(200).json({ message: "success" })
                 } else {
@@ -49,8 +49,8 @@ const FindSingleProfile = async (req, res) => {
 
 const DeleteProfile = async (req, res) => {
     try {
-        const data = await ProfileModel.findOneAndRemove({ _id: req.params.id })
-        res.status(200).json({ message: "deleted" })
+      const data=  await ProfileModel.findOneAndDelete({ _id: req.params.id })
+        res.status(200).json(data)
 
     } catch (error) {
         res.status(404).json(error.message)
