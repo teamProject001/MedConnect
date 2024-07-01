397658a1aed22f00f40c33642942bb313df6afb6
lobna3 <yousfi55lobna@gmail.com>
2024-07-01 12:10:16 +0200

    add

--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router()
 
 const {Register, Login, Test}= require ("../controllers/users.controllers.js")
 
-const {AddProfile,FindAllProfiles,FindSingleProfile,DeleteProfile} = require ("../controllers/profile.controllers.js")
+
 
 router.post('/register',Register)
 router.post('/login',Login)
@@ -12,12 +12,7 @@ router.post('/login',Login)
 router.get('/test',Test)
 
 
-/** Routes Profiles */
 
-router.post('/add',AddProfile)
-router.get('/getProfile',FindAllProfiles)
-router.get('/getProfile/:id',FindSingleProfile)
-router.delete('/deleteProfile/:id',DeleteProfile)
 
 
 module.exports = router;
